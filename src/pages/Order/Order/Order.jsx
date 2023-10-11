8deb12702ffa728535fa203ebe22994833c86b30
shamolmojumder <shamolmojumder14@gmail.com>
2023-10-11 13:29:20 +0600

    75-5 Simple Food Order page with tabs

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import ordercover from "../../../assets/shop/order.jpg";
 import Cover from "../../Shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../hooks/useMenu";
 
 const Order = () => {
+  const [tabIndex,setTabIndex]=useState(0)
+  const[menu] =useMenu();
+  const desserts=menu.filter(item=>item.category==="dessert")
+  const soup=menu.filter(item=>item.category==="soup")
+  const pizza=menu.filter(item=>item.category==="pizza")
+  const salad=menu.filter(item=>item.category==="salad")
+  const offered=menu.filter(item=>item.category==="offered")
   return (
     <div>
       <Cover img={ordercover} title={"Order Food"}></Cover>
-      <div className="tabs">
-        <a className="tab tab-lifted">Tab 1</a>
-        <a className="tab tab-lifted tab-active">Tab 2</a>
-        <a className="tab tab-lifted">Tab 3</a>
-      </div>
+      <Tabs selectedIndex={tabIndex} onSelect={(tabIndex) => setTabIndex(tabIndex)}>
+      <TabList>
+        <Tab>Salad</Tab>
+        <Tab>Pizza</Tab>
+        <Tab>Dessert</Tab>
+        <Tab>Soup</Tab>
+        <Tab>Drinks</Tab>
+        
+      </TabList>
+      <TabPanel></TabPanel>
+      <TabPanel></TabPanel>
+      <TabPanel></TabPanel>
+      <TabPanel></TabPanel>
+      <TabPanel></TabPanel>
+    </Tabs>
     </div>
   );
 };
